a9018a3ac4feacd427efe7da930873a465014422
kkaeduU <eduardogunawan99@gmail.com>
2026-04-01 10:17:01 +0700

    update

--- app/views/students/index.php
+++ app/views/students/index.php
@@ -7,14 +7,37 @@
     <title>Daftar Siswa</title>
     <link rel="stylesheet" href="=/css/output.css">
 </head>
-<body>
+
+<body class="flex flex-col min-h-screen bg-gray-100">
+
     <!-- Header Start -->
     <header class="bg-blue-500 text-white">
-        <div class="flex justify-between container mx-auto"></div>
-            <a href="/students">Sistem Sekolah</a>
-            <a href="/students/create">+ Tambah Siswa</a>
+        <div class="font-bold"flex justify-between container mx-auto p-4"></div>
+            <a class="font-bold" href="/students">Sistem Sekolah</a>
+            <a class="px-2" href="/students/create">+ Tambah Siswa</a>
         </div>
     </header>
      <!-- Header End -->
+
+
+     <!-- Footer Start -->
+<footer class="bg-gray-800 text-white p-4 text-center">
+        2026 - SMK Kristen Immanuel Pontianak - Sistem Sekolah
+</footer>
+     <!-- Footer End -->
+
+
+     <!-- Main Start -->
+<main class="grow container mx-auto">
+<div class="mt-8">
+    <!-- Card Header Start -->
+    <div class="bg-white shadom p-4 rounded-lg">
+        <h1>Daftar Siswa</h1>
+        <p>Menampilkan daftar siswa yang terdaftar</p>
+    </div>
+    <!-- Card Header End -->
+</div>
+</main>
+     <!-- Main End -->
 </body>
 </html>
